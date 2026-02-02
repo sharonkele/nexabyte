012d4790cd46320fc582d39d709302fd8756bc02
sharonkele <sharonodozi2@gmail.com>
2026-02-02 16:31:17 +0000

    add files

---
+++ modules/asg/main.tf
@@ -0,0 +1,31 @@
+resource "aws_launch_template" "foobar" {
+  name_prefix   = "nexabyte-asg"
+  image_id      = var.ami_id
+  instance_type = var.instance_tpe
+  user_data = templatefile("./userdata.sh") 
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups = var.security_groups
+  } 
+
+  block_device_mappings {
+    device_name = "/dev/sda1"
+    ebs {
+        volume_size = 
+        volume_type = 
+        delete_on_termination = 
+    }
+  }
+  }
+
+resource "aws_autoscaling_group" "bar" {
+  availability_zones = ["us-east-1a"]
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.foobar.id
+    version = "$Latest"
+  }
+}
---
+++ modules/asg/variables.tf
@@ -0,0 +1,3 @@
+variable "ami_id" {}
+variable "instance_tpe" {}
+variable "security_group"{}
