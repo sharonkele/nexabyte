9572b873dbe9fa24556400f4f7c088325a4d7bf2
sharonkele <sharonodozi2@gmail.com>
2026-02-06 15:35:21 +0000

    add providers

---
+++ env/dev/versions.tf
@@ -0,0 +1,16 @@
+terraform {
+    required_version = ">=1.0"
+
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = ">=5.12"
+      }
+    }
+
+    backend "s3"{
+        bucket = "terraform-state-so25aco"
+        key = "nexabyte-dev"
+        region = "eu-west-1"
+    }
+}
